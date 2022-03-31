985521f8e1e20425cc5215b832e118fda24e6aaa
Khoa Trương <tnakhoa0905@gmail.com>
2022-04-01 00:43:49 +0700

    cap nhat du lieu Room bang firestore

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hello_flutter/main.dart';
 import 'package:hello_flutter/models/room.dart';
+import 'package:hello_flutter/screens/addroom.dart';
 import 'package:hello_flutter/screens/living_room.dart';
+import 'package:hello_flutter/screens/drawer_slide_menu.dart';
 import 'package:hello_flutter/widgets/room_item.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
-List<Room> _rooms = <Room>[
-  Room('Livingroom', [255, 0, 0, 0], 'assets/images/livingroom.webp',
-      '5 devices', [255, 255, 255, 255]),
-  Room('Kitchen', [255, 254, 246, 229], 'assets/images/kitchen.jpg',
-      '4 devices', [255, 0, 0, 0]),
-  Room('Office ', [255, 233, 231, 247], 'assets/images/iconoffice.png',
-      '10 devices', [255, 0, 0, 0]),
-  Room('Bedroom ', [255, 207, 235, 244], 'assets/images/bedroom.png',
-      '6 devices', [255, 0, 0, 0]),
-  Room('Bathroom ', [255, 250, 235, 232], 'assets/images/bathroom.png',
-      '7 devices', [255, 0, 0, 0]),
-  Room('Dinning ', [255, 239, 247, 229], 'assets/images/dinning.webp',
-      '4 devices', [255, 0, 0, 0]),
-];
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -32,10 +22,15 @@ class Home extends StatefulWidget {
 class _Home extends State<Home> {
   // lay thong tin user
   final user = FirebaseAuth.instance.currentUser!;
+  FirebaseAuth khoa = FirebaseAuth.instance;
+  ScrollController sc = ScrollController();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        key: _scaffoldKey,
+        drawer: BuildDrawer(),
         body: Container(
           padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
           color: const Color.fromARGB(255, 255, 255, 255),
@@ -61,11 +56,19 @@ class _Home extends State<Home> {
           ),
           const Text(
             'Rooms',
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w600,
+            ),
           ),
           const Spacer(),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddRoom()),
+              );
+            },
             style: ElevatedButton.styleFrom(
                 padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 12.0),
                 primary: const Color.fromARGB(255, 226, 246, 245),
@@ -92,17 +95,64 @@ class _Home extends State<Home> {
     );
   }
 
+  final CollectionReference _collectionRef =
+      FirebaseFirestore.instance.collection('rooms');
+
   Widget _buildGridviewRooms(BuildContext context) {
-    return GridView.builder(
-      padding: const EdgeInsets.fromLTRB(36.0, 24.0, 36.0, 24.0),
-      shrinkWrap: true,
-      itemCount: _rooms.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, crossAxisSpacing: 12, mainAxisSpacing: 12),
-      itemBuilder: (context, int index) {
-        return RoomItem(room: _rooms[index]);
-      },
-    );
+    return FutureBuilder<QuerySnapshot>(
+        future: FirebaseFirestore.instance
+            .collection('rooms')
+            .where('idUser', isEqualTo: user.uid)
+            .get(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: Text('Đang tải...'));
+          }
+          final List<DocumentSnapshot> documents = snapshot.data.docs;
+          return GridView.builder(
+            padding: const EdgeInsets.fromLTRB(36.0, 24.0, 36.0, 24.0),
+            shrinkWrap: true,
+            itemCount: documents.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, crossAxisSpacing: 12, mainAxisSpacing: 12),
+            itemBuilder: (BuildContext context, int index) {
+              Room nr = Room(
+                  documents[index].id,
+                  documents[index]['name'],
+                  documents[index]['color'],
+                  documents[index]['avt'],
+                  documents[index]['device'],
+                  documents[index]['textcolor'],
+                  documents[index]['humidity'],
+                  documents[index]['temperature']);
+
+              return RoomItem(room: nr);
+            },
+          );
+        }
+
+        //   return ListView(
+        //       shrinkWrap: true,
+        //       children: documents
+        //           .map((doc) => Card(
+        //                 child: ListTile(
+        //                   title: Text(doc['name']),
+        //                   subtitle: Text(doc['device']),
+        //                 ),
+        //               ))
+        //           .toList());
+        // },
+        // child: GridView.builder(
+        //   padding: const EdgeInsets.fromLTRB(36.0, 24.0, 36.0, 24.0),
+        //   shrinkWrap: true,
+        //   itemCount: _rooms.length,
+        //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //       crossAxisCount: 2, crossAxisSpacing: 12, mainAxisSpacing: 12),
+        //   itemBuilder: (context, int index) {
+        //     return RoomItem(room: _rooms[index]);
+        //   },
+        // ),
+        );
   }
 
   Widget _buildAppbar(BuildContext context) {
@@ -115,17 +165,22 @@ class _Home extends State<Home> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             IconButton(
-              onPressed: () {},
+              onPressed: () => _scaffoldKey.currentState?.openDrawer(),
               icon: const Icon(
                 Icons.grid_view_rounded,
                 size: 40,
               ),
             ),
             const Spacer(),
-            IconButton(
-                // thuc hien dang xuat tai khoan
-                onPressed: () => FirebaseAuth.instance.signOut(),
-                icon: const FaIcon(FontAwesomeIcons.signOutAlt)),
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/images/icon.png'),
+            )
+            // IconButton(
+            //     // thuc hien dang xuat tai khoan
+            //     onPressed: () {
+
+            //     },
+            //     icon: const FaIcon(FontAwesomeIcons.signOutAlt)),
           ],
         ),
       ),
@@ -138,13 +193,14 @@ class _Home extends State<Home> {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           Padding(
-            padding: EdgeInsets.fromLTRB(32.0, 16.0, 0.0, 4.0),
+            padding: const EdgeInsets.fromLTRB(32.0, 16.0, 0.0, 4.0),
             child: Text(
               '${user.displayName}',
-              style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.w600),
+              style:
+                  const TextStyle(fontSize: 35.0, fontWeight: FontWeight.w600),
             ),
           ),
-          Image(image: AssetImage('assets/images/hello.png'))
+          const Image(image: AssetImage('assets/images/hello.png'))
         ],
       ),
       Container(
