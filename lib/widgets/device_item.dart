c5b916e32f99e585d2786466344622fbbda73402
Khoa Trương <tnakhoa0905@gmail.com>
2022-04-01 00:47:33 +0700

    hien thi du lieu va cap nhat status

--- lib/widgets/device_item.dart
+++ lib/widgets/device_item.dart
@@ -1,9 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hello_flutter/models/device.dart';
 
-class ListDevice extends StatelessWidget {
+class DeviceItem extends StatefulWidget {
   final Device devices;
-  const ListDevice({Key? key, required this.devices}) : super(key: key);
+
+  const DeviceItem({Key? key, required this.devices}) : super(key: key);
+
+  @override
+  State<DeviceItem> createState() => _DeviceItem();
+}
+
+class _DeviceItem extends State<DeviceItem> {
+  CollectionReference device =
+      FirebaseFirestore.instance.collection('device_item');
+
+  Future<void> updateStatus() {
+    if (widget.devices.status == 'On') {
+      widget.devices.status = 'Off';
+      return device
+          .doc(widget.devices.id)
+          .update({'status': 'Off'})
+          .then((value) => print('ok rồi'))
+          .catchError((error) => print('Lỗi rồi: $error'));
+    } else {
+      widget.devices.status = 'On';
+      return device
+          .doc(widget.devices.id)
+          .update({'status': 'On'})
+          .then((value) => print('ok rồi'))
+          .catchError((error) => print('Lỗi rồi: $error'));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +40,13 @@ class ListDevice extends StatelessWidget {
       child: SizedBox(
         width: 108,
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              updateStatus();
+            });
+          },
           style: ElevatedButton.styleFrom(
-              primary: Color.fromARGB(devices.color[0], devices.color[1],
-                  devices.color[2], devices.color[3]),
+              primary: Color(widget.devices.color),
               elevation: 0.0,
               shadowColor: Colors.transparent,
               shape: RoundedRectangleBorder(
@@ -26,14 +57,14 @@ class ListDevice extends StatelessWidget {
             children: [
               CircleAvatar(
                 backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-                backgroundImage: AssetImage(devices.img),
+                backgroundImage: AssetImage(widget.devices.img),
               ),
               Text(
-                devices.Name,
+                widget.devices.Name,
                 style: const TextStyle(
                     fontWeight: FontWeight.w800, color: Colors.black),
               ),
-              Text(devices.status,
+              Text(widget.devices.status,
                   style: const TextStyle(
                       fontWeight: FontWeight.w400, color: Colors.black)),
             ],
